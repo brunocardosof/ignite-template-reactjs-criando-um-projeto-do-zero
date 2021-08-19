28d8d02e74574425ec94fea0185da04447ad2b2c
Bruno <bruno.ferreira@southsystem.com.br>
2021-08-19 00:58:48 -0300

    feat: add get static props function to get all posts from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
@@ -25,13 +28,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
-  return <Header />;
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  console.log(postsPagination);
+  return (
+    <>
+      <Head>
+        <title>Desafio 3 - Criando projeto do zero</title>
+      </Head>
+      <Header />
+    </>
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.content', 'posts.subtitle', 'posts.author'],
+      pageSize: 10,
+    }
+  );
 
-//   // TODO
-// };
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: post.data.title[0].text,
+      subtitle: post.data.subtitle[0].text,
+      author: post.data.author,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: { result: posts, next_page: response.next_page },
+    },
+  };
+};
