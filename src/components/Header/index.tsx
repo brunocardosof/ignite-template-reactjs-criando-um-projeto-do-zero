7a040de2ac52d04de4b00e2a8ebc6d49b4f94b19
Bruno <bruno.ferreira@southsystem.com.br>
2021-08-22 18:33:25 -0300

    change header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,13 @@ import styles from './header.module.scss';
 export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
-      <Link href="/">
-        <a>
-          <img src="/images/logo.png" alt="logo" />
-        </a>
-      </Link>
+      <section className={styles.headerContent}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.png" alt="logo" />
+          </a>
+        </Link>
+      </section>
     </header>
   );
 }
