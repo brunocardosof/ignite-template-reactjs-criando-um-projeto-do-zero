8eafe1502b0b0fb9745dd43f19ef1adc42892bf3
Bruno <bruno.ferreira@southsystem.com.br>
2021-08-23 01:16:48 -0300

    feat: add button to load more posts
    add redirect to specific post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import format from 'date-fns/format';
+import { ptBR } from 'date-fns/locale';
 import { RichText } from 'prismic-dom';
 import { FaRegCalendar } from 'react-icons/fa';
 import { FiUser } from 'react-icons/fi';
 
+import { useState } from 'react';
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -31,30 +36,72 @@ interface HomeProps {
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
   const { next_page, results } = postsPagination;
+  const [posts, setPosts] = useState<Post[]>(results || []);
+  const [loading, setLoading] = useState(false);
+  const [nextPage, setNextPage] = useState(next_page);
+
+  async function handleNextPage(): Promise<void> {
+    setLoading(true);
+    await fetch(nextPage)
+      .then(async response => {
+        const result = await response.json();
+        const getPostResult = result.results.map(post => {
+          return {
+            uid: post.id,
+            first_publication_date: format(
+              new Date(post.first_publication_date),
+              'dd MMM yyyy',
+              {
+                locale: ptBR,
+              }
+            ),
+            data: {
+              author: post.data.author[0].text,
+              subtitle: post.data.subtitle[0].text,
+              title: post.data.title[0].text,
+            },
+          };
+        });
+        console.log(getPostResult);
+        setPosts([...posts, ...getPostResult]);
+        setNextPage(result.next_page);
+        setLoading(false);
+      })
+      .catch(error => {
+        setLoading(false);
+        console.log(error);
+      });
+  }
+
   return (
     <>
       <Head>
         <title>Desafio 3 - Criando projeto do zero</title>
       </Head>
       <main className={styles.container}>
-        {results.map(post => (
-          <section className={styles.content} key={post.uid}>
-            <p className={styles.postTitle}>{post.data.title}</p>
-            <p className={styles.postSubtitle}>{post.data.subtitle}</p>
-            <div className={styles.wrapperTimeAuthor}>
-              <time>
-                <FaRegCalendar className={styles.icon} />
-                {post.first_publication_date}
-              </time>
-              <span className={styles.author}>
-                <FiUser className={styles.icon} />
-                {post.data.author}
-              </span>
-            </div>
-          </section>
+        {posts.map(post => (
+          <Link href={`/post/${post.uid}`}>
+            <section className={styles.content} key={post.uid}>
+              <p className={styles.postTitle}>{post.data.title}</p>
+              <p className={styles.postSubtitle}>{post.data.subtitle}</p>
+              <div className={styles.wrapperTimeAuthor}>
+                <time>
+                  <FaRegCalendar className={styles.icon} />
+                  {post.first_publication_date}
+                </time>
+                <span className={styles.author}>
+                  <FiUser className={styles.icon} />
+                  {post.data.author}
+                </span>
+              </div>
+            </section>
+          </Link>
         ))}
-        {next_page && (
-          <span className={styles.loader}>Carregar mais posts</span>
+        {nextPage && (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+          <span className={styles.loader} onClick={handleNextPage}>
+            {loading ? 'Carregando...' : 'Carregar mais posts'}
+          </span>
         )}
       </main>
     </>
@@ -67,19 +114,19 @@ export const getStaticProps: GetStaticProps = async () => {
     [Prismic.predicates.at('document.type', 'posts')],
     {
       fetch: ['posts.title', 'posts.content', 'posts.subtitle', 'posts.author'],
-      pageSize: 10,
+      pageSize: 1,
     }
   );
   const posts = response.results.map((post): Post => {
     return {
       uid: post.id,
-      first_publication_date: new Date(
-        post.first_publication_date
-      ).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-      }),
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
       data: {
         author: post.data.author[0].text,
         subtitle: post.data.subtitle[0].text,
